3c2cafdc63afcca50fbb842cc083b97c2b9ce82b
Julio Renner <juliorenner123@gmail.com>
2018-12-06 16:37:24 -0200

    Update 2.py

--- day05/2.py
+++ day05/2.py
@@ -1,66 +1,43 @@
 import os
-import re
-import collections
-import numpy as np
+import operator
 
 currentDir = os.path.dirname(__file__)
 
 with open(os.path.join(currentDir, 'input.txt'), 'r') as inputFile:
-    input = inputFile.read().split('\n')
+    def removeCharacter(letter):
+        line = input.replace(letter.lower(), '')
+        return line.replace(letter.upper(), '')
 
-    calendar = {}
-
-    for i in input:
-        regex = re.match(r"\[([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)\] (.+)", i)
-
-        year = regex.group(1)
-        month = regex.group(2)
-        day = regex.group(3)
-        hour = regex.group(4)
-        minute = regex.group(5)
-
-        key = year + month + day + hour + minute
-        calendar[int(key)] = i
-
-    minuteMap = {}
-    currentGuard = None
-    asleepMinute = 0
-    for i in sorted(calendar.items()):
-        regex = re.match(r"\[([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)\] (.+)", i[1])
-
-        hour = regex.group(4)
-        minute = regex.group(5)
-
-        action = regex.group(6)
-
-        guardChange = re.match(r".*#([0-9]+)", action)
-        if (guardChange):
-            currentGuard = guardChange.group(1)
-            if (not currentGuard in minuteMap):
-                minuteMap[currentGuard] = np.zeros(60)
-            
+    def countConflicts(currentString):
+        resultString = ""
+        for letter in currentString:
+            if (resultString == ""):
+                resultString += letter
+            else: 
+                lastLetter = resultString[len(resultString)-1]
+                
+                if letter != lastLetter and (letter.lower() == lastLetter.lower()):
+                    resultString = resultString[:-1]
+                else:
+                    resultString += letter
         
-        elif (action == "falls asleep" and currentGuard != None):
-            asleepMinute = int(minute)
-        
-        elif (action == "wakes up"):
-            for r in range(asleepMinute, int(minute)):
-                if (currentGuard != None):
-                    minuteMap[currentGuard][r] = minuteMap[currentGuard][r] + 1
+        return resultString
+
+    input = inputFile.read()
+
+    conflictsMap = {}
+
+    resultString = ""
+    for letter in input:
+        if not letter.upper() in conflictsMap:
+            currentString = removeCharacter(letter)
+            conflictsMap[letter.upper()] = countConflicts(currentString)
     
-    guard = None
-    guardMinuteIndex = 0
-    for i in minuteMap:
-        if (guard == None):
-            guard = i
-            guardMinuteIndex = minuteMap[i].argmax()
-        else:
-            nextGuardMinuteIndex = minuteMap[i].argmax()
-            minutesOfNextGuard = minuteMap[i][nextGuardMinuteIndex]
-            if (minutesOfNextGuard > minuteMap[guard][guardMinuteIndex]):
-                guard = i
-                guardMinuteIndex = nextGuardMinuteIndex
+    highest = None
+    for v in conflictsMap:
+        if highest == None:
+            highest = v
+        elif len(conflictsMap[v]) < len(conflictsMap[highest]):
+            highest = v
 
-    print(int(guard) * guardMinuteIndex)
-
-
+    print("answer: " + str(len(conflictsMap[highest])))
